f7f7fdd3f368070c2a7bcc6cda5cfae1a0dc090b
구자현 <110030247+levhyun@users.noreply.github.com>
2024-09-27 11:16:45 +0900

    etc: Project 생성

---
+++ src/main/kotlin/laundry/amathaapi/AmathaApiApplication.kt
@@ -0,0 +1,11 @@
+package laundry.amathaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmathaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AmathaApiApplication>(*args)
+}
---
+++ src/test/kotlin/laundry/amathaapi/AmathaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package laundry.amathaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmathaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
